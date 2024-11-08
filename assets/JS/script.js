8b6e1d3b37f56e6f894c1b791eee1efd0dc2bc78
Shahan15 <59179902+Shahan15@users.noreply.github.com>
2024-11-08 14:04:08 +0000

    random number generates for questions + comments + submit button fixed

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -3,22 +3,32 @@
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
 
+/*eventlistener for when the submit button is clicked. if submit button clicked alert given. 
+else the other buttons will give the alert based on the button clicked(i.e.gametype - multiplication or division etc.) */
+
     for(let button of buttons) {
         button.addEventListener("click",function() {
             if (this.getAttribute("data-type") === "submit") {
-                alert("you clicked submit!");
+                alert("You Clicked Submit!");
             } else {
                 let gameType = this.getAttribute('data-type');
-                alert(`You clicked ${gameType}`);
+                alert(`You Clicked ${gameType}`);
             }
         })
     }
 })
 
 
+/**
+ * The main game "loop", called when the script is first loaded 
+ * and after the users answer has been processed 
+ */
 
 function runGame () {
 
+    //creates two random numbers between 1 and 24. adds 1 to prevent 0 from being generated 
+    let num1 = Math.floor(Math.random() * 25+) 1
+    let num2 = Math.floor(Math.random() * 25+) 1
 }
 
 function checkAnswer() {
